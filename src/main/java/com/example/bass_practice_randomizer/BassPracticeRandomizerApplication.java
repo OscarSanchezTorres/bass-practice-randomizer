deb912cb4de19177e8d0c29f62f98cb859f0aa3b
Oscar Sanchez <osanchez@foundationmedicine.com>
2023-04-06 13:33:10 +0100

    Adding project boiler plate code

---
+++ src/main/java/com/example/bass_practice_randomizer/BassPracticeRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bass_practice_randomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BassPracticeRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BassPracticeRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bass_practice_randomizer/BassPracticeRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bass_practice_randomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BassPracticeRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
